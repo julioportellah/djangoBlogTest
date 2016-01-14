6061375e2a216e1284fc1a0b26e6a6ad13451db6
julio <julioportellah@gmail.com>
2016-01-14 17:32:08 -0500

    Added new elements

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+	data={'mydata':'Hello wolrd '}
+	#return render(request,'blog/index.html',data)
+	return render(request,'blog/index.html',data)
+#	return HttpResponse("Hello World")
+# Create your views here.
+#def index_troll(request):
+#	return HttpResponse("Troll+LOL=Trolol")
